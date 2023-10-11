0d7035ced9d84b50880900948dac42e94a45a71b
Mruthul M <94045372+mruthul-m@users.noreply.github.com>
2023-10-11 21:30:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+CLIENT_ID = "YOUR CLIENT ID"
+CLIENT_SECRET = "YOUR CLINT SECRET"
+
+REDIRECT_URI = "YOU CAN CREATE A URI TO GENERATE A TOKEN eg: https://example.com/"
+
+# scope means as permissions
+SCOPE = "playlist-read-private playlist-modify-private"
+
+# you get the permission request, grant it.
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        scope=SCOPE,
+        redirect_uri=REDIRECT_URI,
+        show_dialog=True,
+        cache_path="token.txt",
+    )
+)
+
+
+user_id = sp.current_user()["id"]
+
+# Now You can create your own playlist
+NAME = "My-PlayList"
+DESCRIPTION = "GIVE THE DESCRIPTION FOR YOUR PLAYLIST"
+
+playlist = sp.user_playlist_create(
+    user=user_id,
+    name=NAME,
+    public=True,
+    collaborative=False,
+    description=DESCRIPTION,
+)
+
+playlsit_id = playlist["id"]
+
+format = "%Y-%m-%d"
+input("We are Collecting music from the billboard site for the most played 100songs.")
+user_date = input("Please enter your preffered Date in the format YYYY-MM_DD: ")
+
+start = True
+while start:
+    try:
+        bool(datetime.strptime(user_date, format))
+    except ValueError:
+        print("Your format of the date is invalid")
+    else:
+        start = False
+        year = user_date.split("-")[0]
+        billboard_url = f"https://www.billboard.com/charts/hot-100/{user_date}/"
+        response = requests.get(url=billboard_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        song_uris = []
+        songs = soup.select(selector="li #title-of-a-story")
+        for song in songs:
+            track = song.get_text().strip()
+            song_spotify = sp.search(q=f"track:{track} year:{year}", type="track")
+            try:
+                uri = song_spotify["tracks"]["items"][0]["uri"]
+                song_uris.append(uri)
+            except IndexError:
+                print(f"{track} doesn't exist on spotify")
+
+sp.playlist_add_items(playlist_id=playlsit_id, items=song_uris, position=None)
+
+print(f"Your Playlist is ready: https://open.spotify.com/playlist/{playlsit_id}")
---
+++ readme.md
@@ -0,0 +1,13 @@
+Hello 😃,
+This is a simple project for creating your own spotify playlist and adding songs to it.
+
+1. First of all you need a spotify account, If you dont already have an account don't worry,
+   you can signup up for a free one here: [http://spotify.com/signup/](http://spotify.com/signup/)
+
+2. Once you signed up/ signed in, go to the [developer dash board](https://developer.spotify.com/dashboard/)
+
+3. Once you've created a Spotify app, copy the Client ID and Client Secret into the main.py.
+
+4. Authenticating with Spotify is quite complicated, especially when you want to access a user's account. So instead, we're going to use one of the most popular Python Spotify modules - [Spotipy](https://pypi.org/project/spotipy/) to make things easier.
+
+Songs are scrapped in [billboard](https://www.billboard.com)
